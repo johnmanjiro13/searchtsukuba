fcc70c946de6f21a2c6be1e882188329ddf40b3f
johnmanjiro13 <juntama193@gmail.com>
2018-11-19 01:09:09 +0900

    get home timeline

---
+++ main.go
@@ -0,0 +1,26 @@
+﻿package main
+
+import (
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+
+	"github.com/johnmanjiro13/searchtsukuba/keys"
+)
+
+func main() {
+	api := keys.GetTwitterAPI()
+
+	v := url.Values{}
+	v.Set("count", "20")
+
+	timeline, err := api.GetHomeTimeline(v)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	for _, t := range timeline {
+		fmt.Println(t.Text)
+	}
+}
